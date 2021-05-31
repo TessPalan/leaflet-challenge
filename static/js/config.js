81bececbe06ab5de691fefeb6d0c6d5fcc7f7c22
Tess <tesspalan@gmail.com>
2021-05-31 11:55:55 -0700

    logic.js update, almost done

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR pk.eyJ1IjoidGVzc3BhbGFuIiwiYSI6ImNrbmpuNDV6YzAyY3Eyb3A5aWMwOXhxYmQifQ._qWniUxUYqxWUthHTeEs9g";
+const API_KEY = "pk.eyJ1IjoidGVzc3BhbGFuIiwiYSI6ImNrbmpuNDV6YzAyY3Eyb3A5aWMwOXhxYmQifQ._qWniUxUYqxWUthHTeEs9g";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,26 +1,31 @@
 // We create the tile layers that will be the selectable backgrounds of our map.
 
 // Create a L.tilelayer() using the 'mapbox/light-v10' map id
-var grayMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-                              attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+var grayMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                              attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
                               tileSize: 512,
                               maxZoom: 18,
+                              zoomOffset: -1,
                               id: "mapbox/light-v10",
                               accessToken: API_KEY
 });
 
 // Create a L.tilelayer() using the 'mapbox/satellite-v9' map id
-var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-                                  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                                  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+                                  tileSize: 512,
                                   maxZoom: 18,
+                                  zoomOffset: -1,                                  
                                   id: "mapbox/satellite-v9",
                                   accessToken: API_KEY
 });
 
 // Create a L.tilelayer() using the 'mapbox/satellite-v9' map id
-var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-                                attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                                attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+                                tileSize: 512,
                                 maxZoom: 18,
+                                zoomOffset: -1,      
                                 id: "mapbox/outdoors-v11",
                                 accessToken: API_KEY
 });
@@ -31,9 +36,15 @@ var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tile
 
 // Create a L.map(), reference the 'mapid' element in the HTML page, and pass in the three layers above
 var myMap = L.map('mapid', {
+  center: [
+    50, -50
+  ],
+  zoom: 3,
   layers: [grayMap, satelliteMap, outdoorsMap]
 });
 
+// Adding our 'graymap' tile layer to the map.
+grayMap.addTo(myMap);
 
 // We create the layers for our two different sets of data, earthquakes and
 // tectonicplates.
@@ -57,14 +68,13 @@ var baseMaps = {
 // The key should be a human readable name for the layer group, and the value should be a LayerGroup variable
 var overlayMaps = {
   'Tectonic Plates': tectonicPlates,
-  'Earthquakes': earthquakes
+  Earthquakes: earthquakes
 };
 
 // Add a L.control.layers() object and pass in the baseMaps and overlayMaps, and then .addTo myMap
-L.control.layers(
-          baseMaps, overlayMaps, {
-            collapsed: false
-}).addTo(myMap);
+L.control
+  .layers(baseMaps, overlayMaps)
+  .addTo(myMap);
 
 // Use d3.json() to call the API endpoint for earthquake geoJSON data, 
 // .then() fire off an anonymous function that takes a single argument `data`.
@@ -72,8 +82,23 @@ var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&
 
 d3.json(queryUrl, function(data) {
   var earthquakeData = data.features;
+
+  function color(depth) {
+    switch(true) {
+    case depth > 100:
+      return '#9c0909';
+    case depth > 80:
+      return '#d43d0b';
+    case deppth > 60:
+      return '#f09a05';
+    case depth > 40:
+      return '#f0d105';
+    case depth > 20:
+      return '#f0f005';
+    }
+  }
   // Use L.geoJson() to parse the data, and do the following:
-  var earthquakesLayer = L.geoJson(earthquakeData, {
+  L.geoJson(earthquakeData, {
     // use pointToLayer to convert each feature to an L.circleMarker, see https://geospatialresponse.wordpress.com/2015/07/26/leaflet-geojson-pointtolayer/ for a tutorial
       pointToLayer: function(feature, latlng) {
         return L.CircleMarker(latlng, {
@@ -84,7 +109,12 @@ d3.json(queryUrl, function(data) {
     // use style to set the color, radius, and other options for each circleMarker dynamically using the magnitude data
       style: function(feature) {
         return {
-          color: "green"
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: color(feature.geometry.coordinates[2]),
+          color: "#07260a",
+          stroke: true,
+          weight: 0.5
         };
     },
     // use onEachFeature to bind a popup with the magnitude and location of the earthquake to the layer (see above tutorial for an example)
@@ -100,7 +130,31 @@ d3.json(queryUrl, function(data) {
 
   // Create a dynamic legend that describes the color scheme for the circles
   // see this tutorial for guidance: https://www.igismap.com/legend-in-leafletjs-map-with-topojson/
-  // YOUR_CODE_HERE
+  var legend = L.control({position: "bottomright"});
+
+  legend.onAdd = function (myMap) {
+    var div = L.DomUtil.create("div", "info legend")
+    var grades = [0, 20, 40, 60, 80, 100]
+    var colors = [
+      "#026117",
+      "#7fc227",
+      "#1b695a",
+      "#1b4e69",
+      "#01318f",
+      "#10075c"];
+    
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style=”background:"
+         + colors[i]
+         + grades[i]
+         + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+
+   };
+
+  // We add our legend to the map.
+  legend.addTo(myMap);
 
   // BONUS
   // Make another d3.json() call to the tectonic plates API endpoint
